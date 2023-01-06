252af0bfd9abe9bafa58c9eff2bd1f5c6d53327d
ItaloATS <72305895+ITALOTORIBIO@users.noreply.github.com>
2023-01-06 13:12:31 -0500

    Cambios en el fichero Main, comentarios

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        
+        //EDITADO POR ITALO TORIBIO SANTOS
+        
+        
+        
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
         toyo.avanzar();
